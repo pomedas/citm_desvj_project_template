5c05918abfeb61520d7f1a5846c15b70d1c4a2b2
Pedro Omedas <pomedas@gmail.com>
2023-09-07 20:02:03 +0200

    initial commit of clean template

---
+++ Game/Source/Scene.cpp
@@ -0,0 +1,90 @@
+#include "App.h"
+#include "Input.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Render.h"
+#include "Window.h"
+#include "Scene.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+Scene::Scene() : Module()
+{
+	name.Create("scene");
+}
+
+// Destructor
+Scene::~Scene()
+{}
+
+// Called before render is available
+bool Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool Scene::Start()
+{
+	img = app->tex->Load("Assets/Textures/test.png");
+	app->audio->PlayMusic("Assets/Audio/Music/music_spy.ogg");
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::Update(float dt)
+{
+	if(app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		app->render->camera.y -= 1;
+
+	if(app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		app->render->camera.y += 1;
+
+	if(app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		app->render->camera.x -= 1;
+
+	if(app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		app->render->camera.x += 1;
+
+	//Get the size of the window
+	uint windowW, windowH;
+	app->win->GetWindowSize(windowW, windowH);
+
+	//Get the size of the texture
+	uint texW, texH;
+	app->tex->GetSize(img, texW, texH);
+
+	// Renders the image in the center of the screen
+	app->render->DrawTexture(img, windowW /2 - texW / 2, windowH /2 - texH / 2);
+
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+// Called before quitting
+bool Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	return true;
+}
